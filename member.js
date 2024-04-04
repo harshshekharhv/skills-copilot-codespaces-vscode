6d4206ff54a0d7fa8617cf2720e7a14d84638879
Harsh Shekhar <127744514+harshshekharhv@users.noreply.github.com>
2024-04-04 20:03:45 +0000

    Copilot second commit
    Signed-off-by: Harsh Shekhar <127744514+harshshekharhv@users.noreply.github.com>

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberValue = member.options[member.selectedIndex].value;
+  var memberText = member.options[member.selectedIndex].text;
+  var memberSkills = document.getElementById("memberSkills");
+  var memberSkillsText = document.getElementById("memberSkillsText");
+  var memberSkillsValue = document.getElementById("memberSkillsValue");
+  var memberSkillsValueText = document.getElementById("memberSkillsValueText");
+  var memberSkillsValueText2 = document.getElementById("memberSkillsValueText2")
+}
